efa4691b966b9e0fee1c2bf0f8917b83047552b1
RockSolidScripts <ganesh.kugulakrishnan@gmail.com>
2022-01-23 22:52:47 +0530

    User Registration - Added password hashing

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 // * Import NPM Modules
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -14,6 +15,18 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 export const UserModel = mongoose.model("user", UserSchema);
